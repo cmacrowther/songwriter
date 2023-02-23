28668801c863be8bb847b988d82db2240a13cd5c
Colin Crowther <cmacrowther@gmail.com>
2023-02-23 01:03:21 -0400

    fixed promises and token retrieval

---
+++ src/components/SpotifyData.js
@@ -0,0 +1,58 @@
+import React, { Component, PropTypes } from 'react';
+import {decode as base64_decode, encode as base64_encode} from 'base-64';
+import { SpotifyApiContext } from 'react-spotify-api';
+import SongData from "../components/SongData";
+
+const client_id = process.env.GATSBY_SPOTIFY_CLIENT_ID;
+const client_secret = process.env.GATSBY_SPOTIFY_CLIENT_SECRET;
+
+export default class SpotifyData extends Component {
+  constructor(props) {
+    super();
+    this.props = props;
+  }
+
+  state = {
+    token: ""
+  };
+
+  componentDidMount() {
+    this.getData()
+  }
+
+  getData() {
+    Token().then(AuthToken => this.setState({token: AuthToken}));
+  }
+
+  render() {
+    return this.state.token
+        ? 
+        <SpotifyApiContext.Provider value={this.state.token}>
+          <SongData title={this.props.title} />
+        </SpotifyApiContext.Provider>
+        : <div></div>
+  }
+}
+
+async function Token() {
+  const token = await GetAuthTokenAsync();
+  return token;
+}
+
+async function GetAuthTokenAsync() {
+  return fetch('https://accounts.spotify.com/api/token', {
+    method: 'POST',
+    headers: {
+      'Authorization': 'Basic ' + base64_encode(client_id + ':' + client_secret),
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    body: 'grant_type=client_credentials',
+    json: true
+  })
+  .then((response) => response.json())
+  .then(function (credentials) {
+      return credentials.access_token;
+    }).catch((err) => {
+    console.log(err.message);
+  });
+}
--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -3,31 +3,11 @@ import PropTypes from "prop-types";
 import {decode as base64_decode, encode as base64_encode} from 'base-64';
 import { graphql } from "gatsby";
 import { getImage } from "gatsby-plugin-image";
-import { SpotifyApiContext } from 'react-spotify-api';
-
+import SpotifyData from "../components/SpotifyData";
 import Layout from "../components/Layout";
 import Hero from "../components/Hero";
-import SongData from "../components/SongData";
 
 /* eslint-disable */
-
-var client_id = process.env.GATSBY_SPOTIFY_CLIENT_ID;
-var client_secret = process.env.GATSBY_SPOTIFY_CLIENT_SECRET;
-var spotify_token;
-
-console.log(client_id)
-console.log(client_secret)
-
-let callApi = async () => {
-  const result = await this.getSpotifyApiToken();
-  await this.getReq();
-};
-
-/*const songs = [];
-const artists = [];
-tags.skills.map((item)=>{
-  console.log(item.skills.name.en);
-})*/
 
 export const IndexPageTemplate = ({
   image,
@@ -41,10 +21,7 @@ export const IndexPageTemplate = ({
   return (
     <div>
       <Hero title={title} subtitle={subtitle} img={heroImage} color={color} tags={tags} />
-      {spotify_token}
-      <SpotifyApiContext.Provider value={spotify_token}>
-        <SongData title={title} color={color} />
-      </SpotifyApiContext.Provider>
+      <SpotifyData title={title} />
     </div>
   );
 };
@@ -92,22 +69,3 @@ export const pageQuery = graphql`
     }
   }
 `;
-
-async function getSpotifyApiToken() {
-  fetch('https://accounts.spotify.com/api/token', {
-    method: 'POST',
-    headers: {
-        'Authorization': 'Basic ' + base64_encode(client_id + ':' + client_secret),
-        'Content-Type': 'application/x-www-form-urlencoded'
-    },
-    body: 'grant_type=client_credentials',
-    json: true
-  })
-  .then((response) => response.json())
-  .then((data) => {
-    return spotify_token = data.access_token;
-  })
-  .catch((err) => {
-    console.log(err.message);
-  });
-}
