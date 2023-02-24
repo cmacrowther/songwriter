80ef7f463993ce0b632ecb3a334a9e642aa7b13e
Colin Crowther <cmacrowther@gmail.com>
2023-02-23 20:39:19 -0400

    fixed song preview for cms

--- src/cms/preview-templates/SongPagePreview.js
+++ src/cms/preview-templates/SongPagePreview.js
@@ -1,6 +1,5 @@
 import React from 'react'
-import SpotifyData from '../../components/SpotifyData';
-import Layout from '../../components/Layout';
+import "../../components/all.sass";
 
 const SongPagePreview = ({ entry, getAsset }) => {
   const data = entry.getIn(['data']).toJS()
@@ -8,47 +7,45 @@ const SongPagePreview = ({ entry, getAsset }) => {
   if (data) {
     return (
       <div>
-        <Layout>
-          <div className="notification mb-0 is-warning has-text-centered">
-            Song data will be populated below
-          </div>
-          <section class="hero is-fullheight">
-            <div class="hero-body has-text-centered">
-              <div class="container">
-                <div class="box">
-                <div class="card">
-                  <div class="card-image">
-                    <figure class="image is-4by3">
-                      <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image" />
-                    </figure>
-                  </div>
-                  <div class="card-content">
-                    <div class="media">
-                      <div class="media-left">
-                        <figure class="image is-48x48">
-                          <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image" />
-                        </figure>
-                      </div>
-                      <div class="media-content">
-                        <p class="title is-4">John Smith</p>
-                        <p class="subtitle is-6">@johnsmith</p>
-                      </div>
-                    </div>
-
-                    <div class="content">
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                      Phasellus nec iaculis mauris. <a>@bulmaio</a>.
-                      <a href="#">#css</a> <a href="#">#responsive</a>
-                      <br />
-                      <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
-                    </div>
-                  </div>
+        <div className="notification mb-0 is-warning has-text-centered">
+          Song data will be populated below
+        </div>
+        <section class="hero is-fullheight">
+          <div class="hero-body has-text-centered">
+            <div class="container">
+              <div class="box">
+              <div class="card">
+                <div class="card-image">
+                  <figure class="image is-4by3">
+                    <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image" />
+                  </figure>
                 </div>
+                <div class="card-content">
+                  <div class="media">
+                    <div class="media-left">
+                      <figure class="image is-48x48">
+                        <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image" />
+                      </figure>
+                    </div>
+                    <div class="media-content">
+                      <p class="title is-4">John Smith</p>
+                      <p class="subtitle is-6">@johnsmith</p>
+                    </div>
+                  </div>
+
+                  <div class="content">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+                    <a href="#">#css</a> <a href="#">#responsive</a>
+                    <br />
+                    <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+                  </div>
                 </div>
               </div>
+              </div>
             </div>
-          </section>
-        </Layout>
+          </div>
+        </section>
       </div>
     )
   } else {
