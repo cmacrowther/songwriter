c653a2c58d26a1218cf51b998239e412bbcab719
Colin Crowther <cmacrowther@gmail.com>
2023-02-28 14:38:23 -0400

    added gradient effect to layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -12,21 +12,22 @@ export default class Layout extends React.Component{
   }
 
   state = {
-    color: this.children?.props.color
+    color: shadeColor(this.children?.props.color, 20)
   }
 
   listenScrollEvent = e => {
     const title = document.querySelector('.songs');
+    const percentage = 20 - (Math.ceil(20 * (window.scrollY/title.offsetTop)));
 
     if (window.scrollY > title.offsetTop) {
       this.setState({color: '#2b2523'});
     } else {
-      this.setState({color: this.children.props.color});
+      this.setState({color: shadeColor(this.children.props.color, percentage)});
     }
   }
 
   componentDidMount() {
-    this.setState({color: this.children.props.color});
+    this.setState({color: shadeColor(this.children.props.color, 20)});
     window.addEventListener('scroll', this.listenScrollEvent)
   }
 
@@ -70,4 +71,33 @@ export default class Layout extends React.Component{
       </div>
      )
    }
+}
+
+function shadeColor(color, percent) {
+
+  if (color) {
+    var R = parseInt(color.substring(1,3),16);
+    var G = parseInt(color.substring(3,5),16);
+    var B = parseInt(color.substring(5,7),16);
+
+    R = parseInt(R * (100 + percent) / 100);
+    G = parseInt(G * (100 + percent) / 100);
+    B = parseInt(B * (100 + percent) / 100);
+
+    R = (R<255)?R:255;  
+    G = (G<255)?G:255;  
+    B = (B<255)?B:255;  
+
+    R = Math.round(R)
+    G = Math.round(G)
+    B = Math.round(B)
+
+    var RR = ((R.toString(16).length===1)?"0"+R.toString(16):R.toString(16));
+    var GG = ((G.toString(16).length===1)?"0"+G.toString(16):G.toString(16));
+    var BB = ((B.toString(16).length===1)?"0"+B.toString(16):B.toString(16));
+
+    return "#"+RR+GG+BB;
+  } else {
+    return null;
+  } 
 }
