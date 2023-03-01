c926fc3e88e238330ceb792898f8f480994f3358
Colin Crowther <cmacrowther@gmail.com>
2023-03-01 11:36:56 -0400

    fixed footer margins

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,11 +12,11 @@ const Footer = (props) => {
         <div className="content has-background-black has-text-white-ter">
           <div className="container has-background-black has-text-white-ter">
             <div className="columns footer-column pb-5">
-              <div className="column m-4 is-8-touch is-12-mobile">
+              <div className="column footer-item is-8-touch is-12-mobile">
                 <p className="has-text-weight-light has-text-grey-light is-size-6">Powered by <a className="project-link" href="//gitlab.com/cmacrowther/songwriter">Songwriter<FontAwesomeIcon className="pl-1" size="sm" icon={brands('js')} /></a></p>
               </div>
 
-              <div className="column m-4 is-10-mobile has-text-right-desktop has-text-left-mobile social">
+              <div className="column footer-item is-10-mobile has-text-right-desktop has-text-left-mobile social">
                 {
                   props.spotify ? (
                     <a href={props.spotify} className="ml-0">
